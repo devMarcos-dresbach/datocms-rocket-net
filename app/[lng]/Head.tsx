b41061eab939f79629610941c73d695ef5079b19
devMarcos-dresbach <desenvolvedormarcosdresbach@gmail.com>
2024-01-25 08:28:21 -0300

    atualizaçao

--- app/[lng]/Head.tsx
+++ app/[lng]/Head.tsx
@@ -1,11 +1,11 @@
 export default function Head() {
   return (
     <>
-      <title>Landing page DatoCMS Starter</title>
+      <title>Rocket-net</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta
         name="description"
-        content="Visit https://www.datocms.com/marketplace/starters for more starters"
+        content=""
       />
       <link rel="icon" href="/images/favicon.ico" />
     </>
